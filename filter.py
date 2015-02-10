1c25264ba79051386f89040ee52c8462f8d9c421
rwsmith1 <rwsmith1@users.noreply.github.com>
2015-02-09 23:59:02 -0700

    Update filter.py
    Added code to allow passing of raw email text from procmail, as well as parsing for the Subject line to find student name, appointment date, time, and status.

--- filter.py
+++ filter.py
@@ -3,23 +3,69 @@
 # Script only works when run on OSU servers.
 
 import smtplib
+import email
+import sys
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-# sendAddr == my email address
-sendAddr = "do.not.reply@oregonstate.edu"
+# This should allow messages to be piped through by the procmail filter,
+# but I'm not sure how to test it. It works when mail data is entered
+# manually on the console.
+
+fullMsg = sys.stdin.readlines()
+
+inMsg = email.message_from_string(''.join(fullMsg))
 
 # destAddr == recipient's email address
-# Currently set to my email address.
-# Please change if you are going to do any testing.
-destAddr = "smithrog@onid.oregonstate.edu"
+destAddr = inMsg['to']
+sendAddr = inMsg['from']		# Could probably hard code this.
+subject = inMsg['subject']
+advisorName = ""
+apptStatus = ""
+
+# Pulls the body.
+body = inMsg.get_payload()
+bodyParts = body.split('\n')
+
+# This is to split the multiple destination addresses ("REDACTED@.."), if
+# necessary.
+
+# destAddrs = inMsg['to'].split('; ')
+
+# sendAddr == my email address
+# sendAddr = "do.not.reply@oregonstate.edu"
+
+# This parsing is a bit "brute force" for my liking. Anyone have a better
+# ideas how to pull this info?
+
+subjectParts = inMsg['subject'].split(' ')
+
+if subjectParts[0] == "Advising" and subjectParts[1] == "Signup" and subjectParts[2] == "with":
+    advisorName = subjectParts[3] + " " + subjectParts[4];
+    if subjectParts[5] != "confirmed":
+        advisorName += " " + subjectParts[5]
+        apptStatus = subjectParts[6]
+    else:
+        apptStatus = subjectParts[5]
+        
+if subjectParts[0] == "Advising" and subjectParts[1] == "Signup" and subjectParts[2] == "Cancellation":
+    # We should only need both emails, date, and time to cancel an appointment.
+    apptStatus = "cancelled"
+        
+studentName = bodyParts[1].split("Name: ")[1]
+date = bodyParts[3].split("Date: ")[1]
+time = bodyParts[4].split("Time: ")[1]
+
+# Database interaction script call goes here...
 
 # Create message container
 msg = MIMEMultipart('alternative')
-msg['Subject'] = "New Advising Session"
-msg['From'] = sendAddr
+msg['Subject'] = subject
+msg['From'] = sendAddr		# Could probably hard code this.
 msg['To'] = destAddr
+# msg['To'] = destAddrs[1]
 
+# Create iCalendar object.
 mimeText = ""
 calendarRequest = ""  # Variable to hold the calendar request
 # (for when we figure out how to do that.)

f9717851b76abeba9462d1d4594d583d102d5995
harmonh <harmonh@onid.oregonstate.edu>
2015-02-11 13:05:32 -0800

    Update curse.py
    New version includes schedule view

--- curse.py
+++ curse.py
@@ -14,6 +14,7 @@ def main(stdscr):
     userName = sys.argv[1]
     userPass = sys.argv[2]
     results = ""
+    userId = ""
     password = ""
     email = ""
     n = 0
@@ -28,6 +29,7 @@ def main(stdscr):
         cursor.execute(sqlUser)
         results = cursor.fetchone()
         if results is not None:
+            userId = results[0]
             password = results[2]
             email = results[3]
     except:
@@ -53,13 +55,36 @@ def main(stdscr):
         stdscr.addstr(4, 2, '2 - Cancel Appointment')
         stdscr.addstr(5, 2, '3 - Exit')
         stdscr.refresh()
-
         n = stdscr.getch()
+        # view the schedule:
         if n == ord('1'):
             stdscr.clear()
-            stdscr.addstr('Schedule view')
+            stdscr.addstr('Press Any Key for Main Menu')
+            stdscr.addstr(2, 0, 'DATE')
+            stdscr.addstr(2, 15, 'TIME')
+            stdscr.addstr(2, 30, 'STUDENT')
+            stdscr.addstr(2, 50, 'STUDENT EMAIL')
+            sql = "SELECT * FROM Appointment WHERE fkUser = '%d' ORDER BY date" % (userId)
+            cursor.execute(sql)
+            appts = cursor.fetchall()
+            row = 3
+            for app in appts:
+                studentId = app[2]
+                sql = "SELECT name, email FROM Student WHERE pkStudent = '%d'" % (studentId)
+                cursor.execute(sql)
+                student = cursor.fetchone()
+                time = str(app[3])
+                date = str(app[4])
+                #location = app[5]
+                stdscr.addstr(row, 0, date)
+                stdscr.addstr(row, 15, time)
+                stdscr.addstr(row, 30, student[0])
+                stdscr.addstr(row, 50, student[1])
+                row += 1
+
             stdscr.refresh()
             stdscr.getkey()
+        # cancel an appointment
         if n == ord('2'):
             stdscr.clear()
             stdscr.addstr('Cancel view')

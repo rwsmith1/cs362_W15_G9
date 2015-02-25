a9f81c66f3b26893320c585c00a410405628a69e
disatapp <disatapp@gmail.com>
2015-02-25 13:00:31 -0800

    adding sql folder
    Creation and addition queries. The queries for program is located in
    the src/database/databaseEvent.py

---
+++ sql/testquery.sql
@@ -0,0 +1,20 @@
+INSERT INTO User (name, password, email) VALUES 
+  ('user1', '1234', 'user1@gmail.com'),
+  ('user2', '1234', 'user2@gmail.com'),
+  ('user3', '1234', 'user13@gmail.com');
+
+INSERT INTO Student (name, email) VALUES 
+  ('student1', 'student1@test.com'),
+  ('student2', 'student2@test.com'),
+  ('student3', 'student3@test.com');
+
+INSERT INTO Message (fkUser,fkStudent,mailbox) VALUES 
+  ((SELECT pkUser FROM User WHERE name = 'user1' ),(SELECT pkStudent FROM Student WHERE name = 'student2'),'One ring to rule them all'),
+  ((SELECT pkUser FROM User WHERE name = 'user2' ),(SELECT pkStudent FROM Student WHERE name = 'student2'),'One ring to find them'),
+  ((SELECT pkUser FROM User WHERE name = 'user3' ),(SELECT pkStudent FROM Student WHERE name = 'student1'),'One Ring to bring them all and in the darkness bind them')
+  ((SELECT pkUser FROM User WHERE name = 'user1' ),(SELECT pkStudent FROM Student WHERE name = 'student2'),'Val');
+
+INSERT INTO Appointment (fkUser, fkStudent, time, date, location, canceled) VALUES 
+  ((SELECT pkUser FROM User WHERE name = 'user1' ),(SELECT pkStudent FROM Student WHERE name = 'student2'), 132, '2015-02-11', 'online', 0),
+  ((SELECT pkUser FROM User WHERE name = 'user2' ),(SELECT pkStudent FROM Student WHERE name = 'student2'), 132, '2015-12-12', 'Kevins House', 0),
+  ((SELECT pkUser FROM User WHERE name = 'user3' ),(SELECT pkStudent FROM Student WHERE name = 'student1'), 132, '2015-02-14', 'OSU', 0);

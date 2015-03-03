052064bb17c56bcda4708055b7af12ab02168b92
rwsmith1 <rwsmith1@users.noreply.github.com>
2015-03-02 17:28:57 -0700

    Update appointmentBuilder.py

--- src/builders/appointmentBuilder.py
+++ src/builders/appointmentBuilder.py
@@ -1,5 +1,6 @@
 __author__ = 'kkozee'
 
+import datetime
 from src.models.appointment import Appointment
 from src.models.messages import Messages
 
@@ -21,5 +22,20 @@ class AppointmentBuilder(Appointment, Messages):
 
         # We should only need both emails, date, and time to cancel an appointment
         self.student.setName(self.bodyParts[1].split("Name: ")[1])
-        self.setDate(self.bodyParts[3].split("Date: ")[1])
-        self.setTime(self.bodyParts[4].split("Time: ")[1])
+        date = self.bodyParts[3].split("Date: ")[1]
+        timeStr = self.bodyParts[4].split("Time: ")[1]
+        
+        dateParts = date.split(',')
+        dateString = (dateParts[0] + dateParts[1][0:-2] + dateParts[2]).strip()
+        startTimeStr = timeStr.split(" - ")[0]
+        endTimeStr = timeStr.split(" - ")[1]
+
+        startDateTime = datetime.datetime.strptime(startTimeStr + ' ' + dateString, '%I:%M%p %A %B %d %Y')
+        
+        endDateTime = datetime.datetime.strptime(endTimeStr + ' ' + dateString, '%I:%M%p %A %B %d %Y')
+        
+        self.setStartDateTime(startDateTime)
+        
+        self.setEndDateTime(endDateTime)
+        
+        return self

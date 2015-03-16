137e79ccac13daa65fabe2987772812af5c7336f
harmonh <harmonh@onid.oregonstate.edu>
2015-03-16 14:45:37 -0700

    update curse.py

--- curse.py
+++ curse.py
@@ -3,10 +3,10 @@
 import sys
 from src.view.view import View
 
-# get username and password from command line:
+# get username (email) and password from command line:
 args = len(sys.argv)
 if args < 3:
-    sys.exit("Invalid Input: include username and password in command line")
+    sys.exit("Invalid Input: include user-email and password in command line")
 userName = sys.argv[1]
 userPass = sys.argv[2]
 

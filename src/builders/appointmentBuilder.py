15983336c2a2ee6dac2f70aff98ac9ab858db124
harmonh <harmonh@onid.oregonstate.edu>
2015-03-18 20:45:37 -0700

    Cleaned code

--- src/builders/appointmentBuilder.py
+++ src/builders/appointmentBuilder.py
@@ -1,3 +1,11 @@
+####################################################################
+    # Name: appointmentBuilder.py
+    # This class parses a passed email/text message for the adviser's
+    # name, student's name, appointment date, appointment time and
+    # whether the message was for a new appointment or a cancellation.
+    # It takes the information and creates a new appointment in the
+    # database.
+####################################################################
 __author__ = 'kkozee'
 
 import datetime
@@ -7,8 +15,9 @@ from src.models.messages import Messages
 class AppointmentBuilder():
 
     def buildApptFromMessage(self, message):
-
-        if message.subjectParts[0].lower() == "advising" and message.subjectParts[1].lower() == "signup" and message.subjectParts[2].lower() == "with":
+        # Message is for new Appointment
+        if message.subjectParts[0].lower() == "advising" and message.subjectParts[1].lower() == "signup" \
+                and message.subjectParts[2].lower() == "with":
             name = message.subjectParts[5] + " " + message.subjectParts[3].replace(',', '')
             student = message.bodyParts[1].split("Name: ")[1]
             date = message.bodyParts[3].split("Date: ")[1]
@@ -16,31 +25,32 @@ class AppointmentBuilder():
 
             if message.subjectParts[6] == "confirmed":
                 canceled = 0
-
-        elif message.subjectParts[0].lower() == "advising" and message.subjectParts[1].lower() == "signup" and message.subjectParts[2].lower() == "cancellation":
+        # Message is for a Cancelled Appointment
+        elif message.subjectParts[0].lower() == "advising" and message.subjectParts[1].lower() == "signup" \
+                and message.subjectParts[2].lower() == "cancellation":
             name = message.bodyParts[0].split()[5] + " " + message.bodyParts[0].split()[3].replace(',', '')
             canceled = 1
             student = message.bodyParts[1].split("Name: ")[1]
             date = message.bodyParts[3].split("Date: ")[1]
             timeStr = message.bodyParts[4].split("Time: ")[1]
 
+        # Parse date and time from strings
         dateParts = date.split(',')
         dateString = (dateParts[0] + dateParts[1][0:-2] + dateParts[2]).strip()
-
         startTimeStr = timeStr.split(" - ")[0]
         endTimeStr = timeStr.split(" - ")[1]
 
+        # Create new appointment
         appt = Appointment(name, student)
         appt.setCanceled(canceled)
 
         studentEmail = message.bodyParts[2].split('Email: ')[1]
-
         appt.setStudentEmail(studentEmail)
         appt.setUserEmail(message.destAddr)
 
+        # Convert from string to datetime
         startDateTime = datetime.datetime.strptime(startTimeStr + ' ' + dateString, '%I:%M%p %A %B %d %Y')
         endDateTime = datetime.datetime.strptime(endTimeStr + ' ' + dateString, '%I:%M%p %A %B %d %Y')
-        
         appt.setStartDateTime(startDateTime)
         appt.setEndDateTime(endDateTime)
         
